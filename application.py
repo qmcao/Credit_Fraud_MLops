9c2b8d266729b18062b1f69a035b270f08cfd082
Cao, Michael <minh.caoq01@gmail.com>
2025-08-06 18:00:06 -0700

    fix app.py

--- application.py
+++ application.py
@@ -52,7 +52,7 @@ def predict_datapoint():
         return render_template('home.html',results=results)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug = True, host="0.0.0.0", port=8080)
     #test_customdata_prediction()
     
 
