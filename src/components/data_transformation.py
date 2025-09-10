6d6d0f54e62fefb7599386d9dc62fa63a1144951
Cao, Michael <minh.caoq01@gmail.com>
2025-09-09 17:59:14 -0700

    Complete defining sub function in the clean_data processClean_data: drop -> impute -> transformCompleted drop and impute sub function

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -29,19 +29,12 @@ class DataTransformation:
         self.data_transformation_config = DataTransformationConfig()
     
     
-    def find_column_to_drop(self, train_data: pd.DataFrame) -> list:
+    def drop_data(self, train_data: pd.DataFrame, test_data: pd.DataFrame) -> pd.DataFrame:
         """
         Identifies columns in a dataframe with more than 70% missing values.
 
-        The decision is based on the training data to prevent data leakage.
-
-        Args:
-            train_data (pd.DataFrame): The training dataset.
-
-        Returns:
-            list: A list of column names to be dropped.        
+        Drop the corresponding columns from the data
         """
-        
         threshold = 0.7
         columns_to_check = ['prev_address_months_count', 'current_address_months_count', 'intended_balcon_amount', 'bank_months_count', 
                     'session_length_in_minutes', 'device_distinct_emails_8w']  
@@ -51,24 +44,82 @@ class DataTransformation:
         missing_ratio = train_data.isnull().sum() / len(train_data)        
         columns_to_drop = missing_ratio[missing_ratio > threshold].index.tolist()
         
-        return columns_to_drop
+        # Drop the corresponding columns
+        new_train_data = train_data.copy()
+        new_test_data = test_data.copy()
+        new_train_data.drop(columns_to_drop, axis = 1, inplace = True)
+        new_test_data.drop(columns_to_drop, axis = 1, inplace = True)
+        
+        return new_train_data, new_test_data
 
-    
-    def clean_data(self, train_data: pd.DataFrame, test_data: pd.DataFrame, columns_to_drop: list[str]) -> pd.DataFrame:
+    def impute_data(self, train_data: pd.DataFrame, test_data: pd.DataFrame, feature_list: list[str], 
+                    imputer:SimpleImputer, strat:str ) ->pd.DataFrame:
         """
-        This function handle missing data.
+        This function handle impute logic
         """
         
-        columns_to_drop = self.find_column_to_drop(train_data)
-        train = train_data.copy()
-        test = test_data.copy()
+        #Fit on training data then transform on train and test data to prevent leakage
+        Imputer = imputer.set_params(strategy = strat)
+        Imputer.fit(train_data[feature_list])
+
+        #Transform training data
+        train_data_imputed = pd.DataFrame(
+            Imputer.transform(train_data[feature_list]),
+            columns=feature_list,
+            index= train_data.index
+        )
         
-        # Drop corresponding columns
-        train.drop(columns_to_drop, axis = 1, inplace = True)
-        test.drop(columns_to_drop, axis = 1, inplace = True)
+        # Transform testing data
+        test_data_imputed = pd.DataFrame(
+            Imputer.transform(test_data[feature_list]),
+            columns=feature_list,
+            index= test_data.index
+        )
         
-        return train, test
+        return train_data_imputed, test_data_imputed
         
+    def clean_data(self, train_data: pd.DataFrame, test_data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Clean train and test datasets by:
+        
+        1. Dropping columns with >70% missing values.
+        2. Imputing remaining missing values 
+            (median for numeric, most_frequent for categorical).
+        3. (Future) Apply feature transformations.
+
+        Returns cleaned train and test DataFrames.
+        """
+        # Prepare what to impute and impute strategy
+        numerical_features = ['income', 'name_email_similarity', 'current_address_months_count', 'customer_age', 'days_since_request'
+        , 'zip_count_4w', 'velocity_6h', 'velocity_24h', 'velocity_4w', 'bank_branch_count_8w', 
+        'date_of_birth_distinct_emails_4w', 'credit_risk_score', 'bank_months_count', 'proposed_credit_limit',  'session_length_in_minutes',
+        'device_distinct_emails_8w', 'month']
+        
+        cat_features = ['payment_type', 'employment_status', 'housing_status',
+                                'source', 'device_os', 'email_is_free',
+                                    'phone_home_valid',
+                                    'phone_mobile_valid',
+                                    'has_other_cards',
+                                    'foreign_request',
+                                    'keep_alive_session']
+        
+        Imputer = SimpleImputer()
+        numeric_strat = "median" # Highly skewed data
+        cat_strat = "most_frequent"
+        
+        
+        # Start cleaning process : Dropping bad columns -> Impute -> Transform
+        train, test = self.drop_data(train_data, test_data)     
+        imputed_train_numeric, imputed_test_numeric = self.impute_data(train, test,
+                                                                 numerical_features, Imputer, numeric_strat)
+        imputed_train_cat, imputed_test_cat = self.impute_data(train, test,
+                                                                 cat_features, Imputer, cat_strat)
+        
+        # Concat cleaned numerical and categorical data together to return
+        cleaned_train = pd.concat([imputed_train_numeric, imputed_train_cat], axis=1)
+        cleaned_test = pd.concat([imputed_test_numeric, imputed_test_cat], axis=1)
+        
+        return cleaned_train, cleaned_test
     
     def get_data_transformer_object(self, train: pd.DataFrame, test: pd.DataFrame):
         '''
