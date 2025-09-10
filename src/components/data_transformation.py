e4c77d05182b5d4bf3d1c4523de327556fcd993d
Cao, Michael <minh.caoq01@gmail.com>
2025-09-09 22:25:02 -0700

    add DataTrasnformationConfig back to Data transformation processThis make it easier for me to change the config in the future without intefering with the actual Data Transformation logic

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -3,54 +3,96 @@ import pandas as pd
 from sklearn.impute import SimpleImputer 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import RobustScaler, OneHotEncoder, StandardScaler
-
+from dataclasses import dataclass, field
 from src.logger import logging
 
 
+
+@dataclass
+class DataTransformationConfig:
+    # Missing threshold and features
+    drop_missing_threshold: float = 0.70
+    negative_to_nan_cols: list[str] = field(default_factory=lambda:[
+        "prev_address_months_count", "current_address_months_count",
+        "intended_balcon_amount", "bank_months_count",
+        "session_length_in_minutes", "device_distinct_emails_8w",
+    ])
+    
+    # Feature groups
+    numerical_features: list[str] = field(default_factory=lambda:[
+        "income", "name_email_similarity", "current_address_months_count",
+        "customer_age", "days_since_request", "zip_count_4w", "velocity_6h",
+        "velocity_24h", "velocity_4w", "bank_branch_count_8w",
+        "date_of_birth_distinct_emails_4w", "credit_risk_score",
+        "bank_months_count", "proposed_credit_limit",
+        "session_length_in_minutes", "device_distinct_emails_8w", "month",
+    ])
+    categorical_features: list[str] = field(default_factory=lambda: [
+        "payment_type", "employment_status", "housing_status", "source",
+        "device_os", "email_is_free", "phone_home_valid",
+        "phone_mobile_valid", "has_other_cards", "foreign_request",
+        "keep_alive_session",
+    ])
+    
+     # Impute strategies
+    numeric_impute_strategy: str = "median"
+    categorical_impute_strategy: str = "most_frequent"
+    
+    # Enc/Scale params
+    onehot_handle_unknown: str = "ignore"   # important for inference robustness
+    onehot_sparse_output: bool = False
+    scaler: RobustScaler = RobustScaler() 
+       
+
 class DataTransformation:
+    def __init__(self):
+        '''
+        Init data transformation config for the class
+        '''
+        self.transformation_config = DataTransformationConfig()
+        
+    # --- step ---
+        
     def drop_data(self, train_data: pd.DataFrame, test_data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
         Identifies columns in a dataframe with more than 70% missing values.
 
         Drop the corresponding columns from the data
         """
-        threshold = 0.7
-        columns_to_check = ['prev_address_months_count', 'current_address_months_count', 'intended_balcon_amount', 'bank_months_count', 
-                    'session_length_in_minutes', 'device_distinct_emails_8w']  
         
         # Convert negative value to nan and calculate the proportion of missing values for each column
+        columns_to_check = self.transformation_config.negative_to_nan_cols
+        threshold = self.transformation_config.drop_missing_threshold
         train_data[columns_to_check] = train_data[columns_to_check].where(train_data[columns_to_check] >= 0, np.nan)
         missing_ratio = train_data.isnull().sum() / len(train_data)        
         columns_to_drop = missing_ratio[missing_ratio > threshold].index.tolist()
         
         # Drop the corresponding columns
-        new_train_data = train_data.copy()
-        new_test_data = test_data.copy()
-        new_train_data.drop(columns_to_drop, axis = 1, inplace = True)
-        new_test_data.drop(columns_to_drop, axis = 1, inplace = True)
-        
+        new_train_data = train_data.drop(columns_to_drop, axis = 1)
+        new_test_data = test_data.drop(columns_to_drop, axis = 1)
+
         return new_train_data, new_test_data
 
-    def impute_data(self, train_data: pd.DataFrame, test_data: pd.DataFrame, feature_list: list[str], 
-                    imputer:SimpleImputer, strat:str ) -> tuple[pd.DataFrame, pd.DataFrame]:
+    def impute_data(self, train_data: pd.DataFrame, test_data: pd.DataFrame, 
+                    feature_list: list[str], strat:str ) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
         This function handle impute logic
         """
         
         #Fit on training data then transform on train and test data to prevent leakage
-        Imputer = imputer.set_params(strategy = strat)
-        Imputer.fit(train_data[feature_list])
+        imputer = SimpleImputer(strategy=strat)
+        imputer.fit(train_data[feature_list])
 
         #Transform training data
         train_data_imputed = pd.DataFrame(
-            Imputer.transform(train_data[feature_list]),
+            imputer.transform(train_data[feature_list]),
             columns=feature_list,
             index= train_data.index
         )
         
         # Transform testing data
         test_data_imputed = pd.DataFrame(
-            Imputer.transform(test_data[feature_list]),
+            imputer.transform(test_data[feature_list]),
             columns=feature_list,
             index= test_data.index
         )
@@ -89,7 +131,7 @@ class DataTransformation:
         This function scale the numerical variables using Robust Scaler due to large number of outliers
         '''
         # Init scaler object and fit on training set
-        scaler = RobustScaler()
+        scaler = self.transformation_config.scaler
         train = train_data[feature_list]
         test = test_data[feature_list]
         scaler.fit(train)
@@ -121,40 +163,20 @@ class DataTransformation:
 
         Returns cleaned train and test DataFrames.
         """
-        # Prepare what to impute
-        numerical_features = ['income', 'name_email_similarity', 'current_address_months_count', 'customer_age', 'days_since_request'
-        , 'zip_count_4w', 'velocity_6h', 'velocity_24h', 'velocity_4w', 'bank_branch_count_8w', 
-        'date_of_birth_distinct_emails_4w', 'credit_risk_score', 'bank_months_count', 'proposed_credit_limit',  'session_length_in_minutes',
-        'device_distinct_emails_8w', 'month']
         
-        cat_features = ['payment_type', 'employment_status', 'housing_status',
-                                'source', 'device_os', 'email_is_free',
-                                    'phone_home_valid',
-                                    'phone_mobile_valid',
-                                    'has_other_cards',
-                                    'foreign_request',
-                                    'keep_alive_session']
-        #Init impute strat
-        Imputer = SimpleImputer()
-        numeric_strat = "median" # Highly skewed data
-        cat_strat = "most_frequent"
-        
-
         # Start cleaning process : Dropping bad columns -> Impute -> Transform (Encode + Scale)
         # Drop bad columns
         train, test = self.drop_data(train_data, test_data)     
         
         # Impute missing columns
-        imputed_train_numeric, imputed_test_numeric = self.impute_data(train, test, numerical_features, Imputer, numeric_strat)
-        imputed_train_cat, imputed_test_cat = self.impute_data(train, test, cat_features, Imputer, cat_strat)
+        train_num, test_num = self.impute_data(train, test, self.transformation_config.numerical_features,
+                                                                       self.transformation_config.numeric_impute_strategy)
+        train_cat, test_cat = self.impute_data(train, test, self.transformation_config.categorical_features,
+                                                                        self.transformation_config.categorical_impute_strategy)
         
-        # Concat cleaned numerical and categorical data to apply transformation
-        imputed_train = pd.concat([imputed_train_numeric, imputed_train_cat], axis=1)
-        imputed_test = pd.concat([imputed_test_numeric, imputed_test_cat], axis=1)
-        
-        # Encoded categorical columns
-        encoded_train_cat, encoded_test_cat = self.encode_cat_cols(imputed_train, imputed_test,cat_features)
-        scaled_train_num, scaled_test_num = self.scale_num_cols(imputed_train, imputed_test,numerical_features)
+        # Encoded categorical columns, scale numerical
+        encoded_train_cat, encoded_test_cat = self.encode_cat_cols(train_cat, test_cat,self.transformation_config.categorical_features)
+        scaled_train_num, scaled_test_num = self.scale_num_cols(train_num, test_num, self.transformation_config.numerical_features)
         
         # Concat cleaned numerical and categorical data to return 
         cleaned_train = pd.concat([encoded_train_cat, scaled_train_num], axis=1)
